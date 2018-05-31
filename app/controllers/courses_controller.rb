49ccf884baa29ae8bed2df823a491f6b2b2d6af6
Raghu Betina <raghu@firstdraft.com>
2018-05-31 03:47:35 +0000

    Only course user can modify course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,14 @@
 class CoursesController < ApplicationController
+  before_action :current_user_must_be_course_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_course_user
+    course = Course.find(params[:id])
+
+    unless current_user == course.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @courses = Course.all
 
