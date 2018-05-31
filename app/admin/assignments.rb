cba5e213c8ead97c8978a5cb991b71be37b92ea3
Raghu Betina <raghu@firstdraft.com>
2018-05-31 03:47:28 +0000

    Whitelist columns in Assignment for ActiveAdmin

--- app/admin/assignments.rb
+++ app/admin/assignments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Assignment do
+
+ permit_params :course_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
