cfee76de9c973cccb94da9c5ac46c74f3a042571
Raghu Betina <raghu@firstdraft.com>
2018-05-31 03:47:35 +0000

    Only course user can access show page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :current_user_must_be_course_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_course_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_course_user
     course = Course.find(params[:id])
